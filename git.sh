9bd0de492f94c6352c3b13fb5dcbc18e83956418
Azabell1993 <Azabell1993>
2024-07-12 13:45:28 +0900

    first

--- git.sh
+++
@@ -1,7 +0,0 @@
-sudo echo "# UbuntuSetting_v2" >> README.md
-sudo git init
-sudo git add README.md
-sudo git commit -m "first commit"
-sudo git branch -M main
-sudo git remote add origin https://github.com/Azabell1993/UbuntuSetting_v2.git
-sudo git push -u origin main
